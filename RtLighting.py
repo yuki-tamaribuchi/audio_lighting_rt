c1634ae4d061d48ee78d3634cacbc305e3fb26c8
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-12 13:37:31 +0900

    color関数の実装

--- RtLighting.py
+++ RtLighting.py
@@ -14,8 +14,71 @@ class RtLighting():
 
 
     def __color(self,y):
-        chroma_srft=librosa.feature.chroma_stft(y=y,sr=44100)
 
+        chroma_rgb=np.array([
+            #Kari Ziets' research 1931
+            #Color Name to RGB Reference -> https://web.njit.edu/~walsh/rgb.html
+
+            #C,ド,Red
+            [255,0,0],
+
+            #C#,ド#(レb),Purple
+            [160,32,240],
+
+            #D,レ,Violet
+            [238,130,238],
+
+            #D#,レ#(ミb),LightBlue
+            [173,216,230],
+
+            #E,ミ,Gold
+            [255,215,0],
+
+            #F,ファ,Pink
+            [255,192,203],
+
+            #F#,ファ#(ソb),turquoise4
+            [0,134,139],
+
+            #G,ソ,SkyBlue
+            [135,206,235],
+
+            #G#,ソ#(ラb),Unknown -> mean of G and A
+            [195,230,79],
+
+            #A,ラ,冷たい黄 -> Yellow
+            [255,255,0],
+
+            #A#,ラ#(シb),Orange
+            [255,165,0],
+
+            #B,シ,Copper
+            [184,115,51],
+
+            #None,WHite
+            [255,255,255]
+        ])
+
+        def convert_rgb_to_xy(data):
+            r_gamma = pow( ((data[0]/256) + 0.055) / (1.0 + 0.055), 2.4 ) if (data[0]/256) > 0.04045 else ((data[0]/256) / 12.92)
+            g_gamma = pow( ((data[1]/256) + 0.055) / (1.0 + 0.055), 2.4 ) if (data[1]/256) > 0.04045 else ((data[1]/256) / 12.92)
+            b_gamma = pow( ((data[2]/256) + 0.055) / (1.0 + 0.055), 2.4 ) if (data[2]/256) > 0.04045 else ((data[2]/256) / 12.92)
+
+            x = r_gamma * 0.649926 + g_gamma * 0.103455 + b_gamma * 0.197109
+            y = r_gamma * 0.234327 + g_gamma * 0.743075 + b_gamma * 0.022598
+            z = g_gamma * 0.053077 + b_gamma * 1.035763
+
+            x=x/(x+y+z)
+            y=y/(x+y+z)
+
+            return x,y
+
+        chroma_srft=librosa.feature.chroma_stft(y=y,sr=44100)
+        xy=convert_rgb_to_xy(chroma_rgb[np.append(chroma_stft.real.mean(axis=1),[0.00000000001]).argmax()])
+
+        return xy
+
+        
     def __brightness(self):
         pass
 
